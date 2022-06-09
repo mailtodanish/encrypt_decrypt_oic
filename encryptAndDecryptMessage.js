b95ded79ced8823c8999fe9aaefe984429f35d26
Mohd Ahshan Danish <mailtodanish@gmail.com>
2022-06-09 10:50:57 +0530

    Update encryptAndDecryptMessage.js

--- encryptAndDecryptMessage.js
+++ encryptAndDecryptMessage.js
@@ -1,9 +1,9 @@
 function encrypt_decrypt(method, message, secratekey) {
-    var responseVar = "";
     if (method.toUpperCase() == "ENCRYPT") {
-        responseVar = CryptoJS.AES.encrypt(message, secratekey);
+        return CryptoJS.AES.encrypt(message, secratekey);
     } else if (method.toUpperCase() == "DECRYPT") {
-        responseVar = CryptoJS.AES.decrypt(message, secratekey).toString(CryptoJS.enc.Utf8);
+        return CryptoJS.AES.decrypt(message, secratekey).toString(CryptoJS.enc.Utf8);
     }
-    return responseVar;
+    
+    return ""
 }
